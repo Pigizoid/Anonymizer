1cfd81ca5fd6e67fb06bc44598cd90e008059fd1
Pigizoid <evansimpson470@gmail.com>
2025-08-13 11:15:30 +0800

    loaded in main CLI file
    loaded in MVP main CLI file

---
+++ main.py
@@ -0,0 +1,92 @@
+
+import typer
+
+from functions.synthesiser import Synthesiser
+import schema
+
+from functions.anonymiser import Anonymiser
+
+
+import yaml
+import json
+
+
+app = typer.Typer()
+
+def synth_func(schema_model,method,amount,file,start_index=0):
+	dataset = Synthesiser.synthesise(schema_model,method,amount)  #returns as [ data, data, ... ]
+	flush = []
+	for index,data in enumerate(dataset):
+		if file != None:
+			flush.append(f"{index+1+start_index}: {json.dumps(dict(data),indent=8)}")
+		else:
+			flush.append(f"{index+1+start_index}: {data}")
+		if (index+1)%max((amount//10),1) == 0:
+			if file != None:
+				flush_out = ",\n    ".join(flush)
+				with open(f"{file}.json","a") as f:
+					f.write(flush_out)
+			else:
+				flush_out = "\n".join(flush)
+			print(flush_out)
+			flush.clear()
+	if flush != []:
+		if file != None:
+			flush_out = ",\n    ".join(flush)
+			with open(f"{file}.json","a") as f:
+				f.write(flush_out)
+		else:
+			flush_out = "\n".join(flush)
+		print(flush_out)
+		flush.clear()
+	if file != None:
+		print(f"To file -> {file}")
+
+
+@app.command()
+def synthesise(method:str = "faker",amount:int=1,file:str=None,batch:int=0):
+	
+	if file != None:
+		with open(f"{file}.json","w") as f:
+			f.write("[")
+	
+	schema_model = schema.Address
+	if batch != 0:
+		batch_index = 0
+		for y in range(amount//batch):
+			print("Batch: ",y+1)
+			synth_func(schema_model,method,batch,file,start_index=batch_index)
+			batch_index+=batch
+		synth_func(schema_model,method,amount-batch_index,file,start_index=batch_index)
+	else:
+		synth_func(schema_model,method,amount,file)
+	if file != None:
+		with open(f"{file}.json","a") as f:
+			f.write("\n]")
+
+
+@app.command()
+def anonymise(data:str, method:str = "faker",config: str = None):
+	
+	with open(data) as dt_file: 
+		try:
+			data = json.load(dt_file)
+		except Exception as e:
+			print(e)
+			data = {}
+	
+	if config != None:
+		
+		with open(config) as cf_file:
+			try:
+				#print(yaml.safe_load(cf_file))
+				config = dict(yaml.safe_load(cf_file))
+			except yaml.YAMLError as exc:
+				print(exc)
+	
+	schema_model = schema.Address
+	data = Anonymiser.anonymise(schema_model,method,data,config)
+    
+
+if __name__ == "__main__":
+    app()
