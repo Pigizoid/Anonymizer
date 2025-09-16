9a711d2f5bf179ebfa7649c417f31868912ad966
Pigizoid <evansimpson470@gmail.com>
2025-09-16 16:17:48 +0800

    update: removed Field(default=...) and set as just = ...
    Field(default=...) and = ... are the same when Field has no extra values, its better to remove Field to avoid excessive function calls

--- src/sm/app/models.py
+++ src/sm/app/models.py
@@ -1,31 +1,31 @@
 # app_models.py
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 import typing
 
 
 class SynthesiserConfig(BaseModel):
-    method: str = Field(default="mixed")
-    amount: int = Field(default=1)
-    batch: int = Field(default=0)
-    output: str = Field(default="")
-    cout: bool = Field(default=False)
+    method: str = "mixed"
+    amount: int = 1
+    batch: int = 0
+    output: str = ""
+    cout: bool = False
 
 
 class AnonymiserConfig(BaseModel):
-    ingest: str = Field(default="")
-    method: str = Field(default="mixed")
-    amount: int = Field(default=1)
-    start: int = Field(default=0)
-    output: str = Field(default="")
-    cout: bool = Field(default=False)
-    manual: bool = Field(default=False)
-    default: typing.Literal["mask", "synth", "perturb"] = Field(default="mask")
-    fields: typing.Dict[str, str] = Field(default={})
+    ingest: str = ""
+    method: str = "mixed"
+    amount: int = 1
+    start: int = 0
+    output: str = ""
+    cout: bool = False
+    manual: bool = False
+    default: typing.Literal["mask", "synth", "perturb"] = "mask"
+    fields: typing.Dict[str, str] = {}
 
 
 class Settings(BaseSettings):
     schema_path: typing.Optional[str]
-    seed: typing.Union[int, str, bool, None] = Field(default=False)
+    seed: typing.Union[int, str, bool, None] = False
     synth: typing.Optional[SynthesiserConfig]
     anon: typing.Optional[AnonymiserConfig]
