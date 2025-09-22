65616916017a320217b25e5546e58422c3f6f189
Evan Simpson <140896920+Pigizoid@users.noreply.github.com>
2025-09-22 11:12:38 +0800

    Merge Dataclass functionality into DEV (#23)
    * edit(wip): moved functions and removed relative imports
    
    * edit: toml file and pytest imports and testing directory
    
    * fix: fixed and removed `guess_type()` and related code
    
    * fix: remove os.chdir from tests
    
    fixed testing environment
    removed os.chdir
    removed filepath parent.parent calls
    set "tests\\" for pytest inputs
    fixed CLI default values loading improperly
    
    * update: removed Field(default=...) and set as just = ...
    
    Field(default=...) and = ... are the same when Field has no extra values, its better to remove Field to avoid excessive function calls
    
    * fix: docstrings formatting, input data types, output data types
    
    * bug: fixed anonymiser
    
    field inputs mismatch
    
    * Delete main(old).py
    
    * dataclass: minimal dataclass
    
    to be fixed: running as a dataclass, currently it only runs in the file
    
    * update: allow pydantic fields as input into SynthField
    
    * edit: fix, complex pattern constraints on small amounts
    
    before: only used lookaround regex on amounts larger than 10000, now uses it if lookaround found in pattern
    
    * Update dataclass.py
    
    * update: dataclass update and dataclass tests
    
    * small update
    
    moved import to only import when run from main

---
+++ src/sm/library/dataclass.py
@@ -0,0 +1,104 @@
+
+from dataclasses import dataclass, field
+from typing import Any, List, Dict, Type, Annotated, Union
+if __name__ == "__main__":
+    import os,sys,pathlib
+    sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent.parent))
+from sm.anonymiser.handling.data_handling import anonymise_data
+from sm.synthesiser.synthesiser import Synthesiser
+from sm.tools.model_funcs import get_model_fields
+from pydantic import BaseModel, Field
+from pydantic.fields import FieldInfo
+
+
+'''
+functionality:
+    amount = 10
+    data = dict(
+        foo=AnonField("foo",amount,"mask","hello"),
+        bar=AnonField("bar",amount,"perturb",10),
+        zar=AnonField("zar",amount,"mask",True),
+        lar=SynthField("name",amount,"faker",fields["name"]),
+        nest=AnonField("nest",amount,"mask",{"field1":"bob"})
+    )
+
+    data['foo'].type -> str
+    data['foo'].vals -> ["hrllo","ello","ghidi","*****",...]
+    data['foo'].method -> "mask"
+
+    data['lar'].type -> str
+    data['lar'].vals -> ["john","larry","penny",...]
+    data['lar'].method -> "faker"
+'''
+
+
+
+@dataclass
+class SMField:
+    field_name: str
+    amount: int
+    method: str
+    type: Type = field(init=False)
+    vals: List[Any] = field(init=False, default_factory=list)
+
+    def __post_init__(self):
+        self.type = type(self.value)
+
+
+
+@dataclass
+class AnonField(SMField):
+    value: Any
+
+    def __post_init__(self):
+        self.type = type(self.value)
+        if self.method == "synth":
+            raise ValueError("Method 'synth' is not available for AnonField, use SynthField instead")
+        self.vals = [anonymise_data(self.value,(self.field_name,self.method)) for _ in range(self.amount)]
+        
+
+@dataclass
+class SynthField(SMField):
+    field_info : FieldInfo
+
+    def __post_init__(self):
+        self.synth = Synthesiser(method=self.method)
+        self.match_name = self.synth.match_fields([self.field_name])[self.field_name]
+        self.applied_constraints = self.synth.check_generation_constraints(self.field_name,self.field_info)
+        self.type = self.applied_constraints["annotation"]
+
+        self.vals = [self.synth.generate_synth_data(self.field_name, self.match_name, self.applied_constraints, f"{self.field_name}(?)[{self.amount}]") for _ in range(self.amount)]
+        #self.vals = [self.synth.generate_single_value(self.field_name,self.type) for _ in range(self.amount)]
+
+
+class User(BaseModel):
+    name: str = Field(pattern=r'[A-Z]{1,1}[a-z]{1,10}')
+    email: str
+    age: int = Field(ge=10,le=100)
+
+
+if __name__ == "__main__":
+    fields = get_model_fields(User)
+    amount = 10
+    #usage 1
+    data = dict(
+        foo=AnonField("foo",amount,"mask","hello"),
+        bar=AnonField("bar",amount,"perturb",10),
+        zar=AnonField("zar",amount,"mask",True),
+        lar=SynthField("name",amount,"faker",fields["name"]),
+        nest=AnonField("nest",amount,"mask",{"field1":"bob"})
+    )
+    for z in range(amount):
+        print(dict({x:data[x].vals[z] for x in data.keys()}))
+
+    #usage 2
+    data = {
+        field_name : SynthField(field_name,amount,"faker",field_data) 
+        for field_name,field_data in fields.items()
+        }
+
+    for z in range(amount):
+        print(dict({x:data[x].vals[z] for x in data.keys()}))
+
+
+
