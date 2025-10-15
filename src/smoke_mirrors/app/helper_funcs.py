c9e7ee6cfdc8fe4e70e741c12e45f64f4622bd2a
Pigizoid <evansimpson470@gmail.com>
2025-10-15 14:25:20 +0800

    update: test fix 3

--- src/smoke_mirrors/app/helper_funcs.py
+++ src/smoke_mirrors/app/helper_funcs.py
@@ -408,7 +408,7 @@ def return_flags(ctx, config_schema:BaseModel):
 
 
 def windows_path_to_pathlib(path_str:Union[str,Path]) -> Path:
-    if isinstance(path_str,Path):
+    if isinstance(path_str,type(Path("nothing"))):
         print(f"putting in a path {str(path_str)}")
         return path_str
     win_path = PureWindowsPath(str(path_str))
